beb0e51514dd6613fe2bbd9158cdecc6b67a2257
Mross2078 <matthew.ross2078@gmail.com>
2015-09-14 20:20:10 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,58 @@
+var Hapi = require('hapi');
+var Path = require('path');
+var http = require('http');
+var request = require('request');
+var Handlebars = require("handlebars");
+
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.argv[2] || 8888)
+});
+
+server.views({
+		path: 'templates',
+		engines: {
+			html: require('handlebars')
+		},
+		relativeTo:__dirname
+});
+
+server.route([
+	{
+	method: "GET",
+	path: "/",
+	handler: function(request, reply) {
+		reply.file("./templates/homepage.html");
+		}
+	},
+	{
+	method: "GET",
+	path: "/dynamic.js",
+	handler: function(request, reply) {
+		// data: JSON.stringify(request.payload),
+		//console.log(JSON.stringify(request.payload));
+		//console.log(request);
+		reply.file("./templates/dynamic.js")
+
+		}
+	},
+	{
+	method: "GET",
+	path: "/style.css",
+	handler: function(request, reply) {
+		reply.file("./templates/style.css");
+		}
+	},
+	{
+	method:"GET",
+	path: "/venstar_themes1_2.jpg",
+	handler: function(request, reply) {
+		reply.file("./templates/venstar_themes1_2.jpg");
+	}	
+	}
+]);
+
+server.start();
+console.log("Mr. Ross, your server is currently running on port 8888.");
